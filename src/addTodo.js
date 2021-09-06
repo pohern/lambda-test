d56d8c4677dd360ea22420895cde3a43c7d0efc7
pohern <ohern_p@yahoo.com>
2021-09-06 15:59:24 -0500

    Add function to create a todo;

---
+++ src/addTodo.js
@@ -0,0 +1,30 @@
+const { v4 } = require('uuid')
+const AWS = require('aws-sdk');
+
+
+const addTodo = async (event) => {
+  const dynamoDB = AWS.DynamoDB.DocumentClient
+
+  const { todo } = JSON.parse(event.body);
+  const createdAt = new Date();
+  const id = v4();
+  const newTodo = {
+    id,
+    todo,
+    createdAt,
+    completed: false
+  }
+  dynamoDB.put({
+    TableName: 'TodoTable',
+    Item: newTodo
+  })
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(newTodo),
+  };
+};
+
+module.exports = {
+  handler: addTodo 
+}
